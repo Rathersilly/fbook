41f79d107268a4759870c4f01262ceca87964c97
rathersilly <rathersilly@live.com>
2021-04-07 07:23:30 -0600

    homepage and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get '/help', to: 'static_pages#help', as: 'help'
+  get '/home', to: 'static_pages#home', as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
